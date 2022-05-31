73f9ea611cd9c54a0f685c6a86379a1b6fcb69ee
David Graham <prograhammer@gmail.com>
2022-05-30 22:18:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 8080;
+const __dirname = path.resolve();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
