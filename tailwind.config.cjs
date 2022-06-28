28264c978d49cdb90369116174cf989b4fdb66da
David Graham <prograhammer@gmail.com>
2022-06-27 21:56:01 -0500

    added bowl

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': ['pixel2', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
